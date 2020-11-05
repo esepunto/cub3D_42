5ac6ae8b962ecb7605264b0bba75ca1cf4308534
esepunto <esepunto@gmail.com>
2020-11-05 07:16:59 +0100

    Sort structures

--- srcs/2read_map/read_map.c
+++ srcs/2read_map/read_map.c
@@ -12,7 +12,7 @@
 
 #include "../../includes/cub3d.h"
 
-int		read_map(char *name_map, t_all *a)
+int		read_map(char *name_map, t_cub3d *a)
 {
 	int			fd;
 	char		*line;
@@ -31,18 +31,18 @@ int		read_map(char *name_map, t_all *a)
 	while ((i = get_next_line(fd, &line)) > 0)
 	{
 		columnas = ft_strlen(line);
-		if (columnas > a->columna_mayor)
-			a->columna_mayor = columnas;
+		if (columnas > a->map.columna_mayor)
+			a->map.columna_mayor = columnas;
 		free(line);
 		line = NULL;
-		a->filas++;
+		a->map.filas++;
 	}
 	free(line);
 	line = NULL;
 	close(fd);
 
 
-	if (!(a->map = (char **)malloc(a->filas * sizeof(char *))))
+	if (!(a->.map.map = (char **)malloc(a->map.filas * sizeof(char *))))
 		return (-1);
 	i = 0;
 	if ((fd = open(name_map, O_RDONLY)) == -1)
@@ -54,7 +54,7 @@ int		read_map(char *name_map, t_all *a)
 	c = 0;
 	while ((i = get_next_line(fd, &line)) > 0)
 	{
-		a->map[c] = ft_strdup(line);
+		a->map.map[c] = ft_strdup(line);
 		c++;
 		free(line);
 		line = NULL;
@@ -65,22 +65,22 @@ int		read_map(char *name_map, t_all *a)
 	return (0);
 }
 
-void	init_struct(t_all *a)
+void	init_struct(t_cub3d *a)
 {
-	a->map = NULL;
-	a->filas = 0;
-	a->columna_mayor = 0;
+	a->map.map = NULL;
+	a->map.filas = 0;
+	a->map.columna_mayor = 0;
 }
 
 int		main(int argc, char **argv)
 {
-	t_all	a;
+	t_cub3d	a;
 	int		i;
 	int		len;
 
 	len = 0;
 	
-	printf("Tamaño estructura antes iniciar: %lu\n", sizeof(t_all));
+	printf("Tamaño estructura antes iniciar: %lu\n", sizeof(t_cub3d));
 	init_struct(&a);
 	printf("Tamaño estructura antes iniciar: %lu\n", sizeof(a));
 	if (argc == 2)
@@ -89,27 +89,27 @@ int		main(int argc, char **argv)
 	}
 	else
 		printf("Please, type carefully. It looks there's an error in you order. Thanks!\n");
-	printf("filas: %d\n", a.filas);
-	printf("colum: %zu\n", a.columna_mayor);
+	printf("filas: %d\n", a.map.filas);
+	printf("colum: %zu\n", a.map.columna_mayor);
 	i = 0;
-	printf("map[0][7] = %c\n", a.map[0][7]);
-	while (i < a.filas)
+	printf("map[0][7] = %c\n", a.map.map[0][7]);
+	while (i < a.map.filas)
 	{
-		printf("linea %d: %s\n", i, a.map[i]);
+		printf("linea %d: %s\n", i, a.map.map[i]);
 		i++;
 	}
-	printf("map[0][0] = %c", a.map[0][0]);
+	printf("map[0][0] = %c", a.map.map[0][0]);
 
 	i = 0;
-/*	if (a.map)
+/*	if (a.map.map)
 	{
-		printf("\nHay a.map.\n");
-		while (i < a.filas)
+		printf("\nHay a.map.map.\n");
+		while (i < a.map.filas)
 		{
-			free(a.map[i]);
+			free(a.map.map[i]);
 			i++;
 		}
-		free(a.map);
+		free(a.map.map);
 	}
 */	system("leaks cub3D");
 }
