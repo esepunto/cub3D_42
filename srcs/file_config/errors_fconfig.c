253193bf49fb2431307622acc9a0de595ede4ac9
Samuel Sacristan sanz <ssacrist@c1r1s2.42madrid.com>
2020-11-05 23:44:19 +0100

    Backup of very little changes. Its Compile in MacOs

--- srcs/file_config/errors_fconfig.c
+++ srcs/file_config/errors_fconfig.c
@@ -6,7 +6,7 @@
 /*   By: ssacrist <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/05 18:57:34 by ssacrist          #+#    #+#             */
-/*   Updated: 2020/11/05 18:57:34 by ssacrist         ###   ########.fr       */
+/*   Updated: 2020/11/05 23:42:37 by ssacrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	errors_mgmt(char *id, t_cub3d *a, int c)
 	int		j;
 
 	a->fconf.wallno = (look4_id("NO ", a, 0));
-	
+
 	aux = a->fconf.res;
 	i = 0;
 	j = a->fconf.init_id[c];
