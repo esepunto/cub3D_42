3a18900ea6e1d6863a5ef86577bb79afa00352b6
Samuel Sacristan sanz <ssacrist@c1r1s5.42madrid.com>
2020-11-09 18:05:05 +0100

    Stop and error if 2 players

--- srcs/file_config/map/manage_map.c
+++ srcs/file_config/map/manage_map.c
@@ -6,11 +6,29 @@
 /*   By: ssacrist <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/08 10:32:12 by ssacrist          #+#    #+#             */
-/*   Updated: 2020/11/09 17:12:46 by ssacrist         ###   ########.fr       */
+/*   Updated: 2020/11/09 18:04:54 by ssacrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/cub3d.h"
+
+/*
+** Add chars N, S, W and E from map.
+** If there's more than one, return error because
+** not accept more than one player.
+*/
+
+static void	repeat_chr(char c)
+{
+	static int count;
+
+	if (!count)
+		count = 0;
+	if (c == 'N' || c == 'S' || c == 'W' || c == 'E')
+			count++;
+	if (count > 1)
+		msg_err("Sorry, I can't accept two players.");
+}
 
 void	forbidd_chr(char c)
 {
@@ -38,20 +56,24 @@ void 	review_map_horiz(t_cub3d *a, int i, int j)
 	imax = a->fconf.map.row -1;
 	jmax = a->fconf.map.col -1;
 	map = a->fconf.map.map[i];
-	printf("imax: |%d|\n", imax);
-	printf("jmax: |%d|\n", jmax);
-	printf(" map: |%s|\n", map);
-	printf("   i: |%d|\n", i);
-	printf("   j: |%d|\n", j);
-
+//	printf("imax: |%d|\n", imax);
+//	printf("jmax: |%d|\n", jmax);
+//	printf(" map: |%s|\n", map);
+//	printf("   i: |%d|\n", i);
+//	printf("   j: |%d|\n", j);
 	while (i <= imax)
 	{
 		map = a->fconf.map.map[i];
 		j = 0;
 		while (j <= jmax && map[j] != '\0')
 		{
-			if (ft_isdigit(map[j]) == 0)
+			if (ft_isprint(map[j]) == 1)
+			{
+//				printf("map[%d]  pre: |%c|\n", j, map[j]);
 				forbidd_chr(map[j]);
+//				printf("map[%d] post: |%c|\n", j, map[j]);
+				repeat_chr(map[j]);
+			}
 			j++;
 		}
 		i++;
