a970a415ddf38601ae3a88b736fac0994f66320b
Samuel Sacristan sanz <ssacrist@c1r1s5.42madrid.com>
2020-11-09 21:12:23 +0100

    It looks that algorithm to invalid maps is OK

--- srcs/file_config/map/manage_map.c
+++ srcs/file_config/map/manage_map.c
@@ -6,11 +6,63 @@
 /*   By: ssacrist <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/08 10:32:12 by ssacrist          #+#    #+#             */
-/*   Updated: 2020/11/09 18:04:54 by ssacrist         ###   ########.fr       */
+/*   Updated: 2020/11/09 21:10:07 by ssacrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/cub3d.h"
+
+/*
+**  The algorith to found invalid maps its based.
+** 
+**  There is an absolute truth to know if a map is closed: in each and every 
+**  line of the **  map (both horizontal and vertical): before an empty space
+**  there will be a wall (1), and the next site (not space) there will be a
+**  wall (1).
+**
+**  There are 2 exceptions: when a line begins and/or finishes with a wall (1),
+**  because there cannot be a space before (when begins) nor space after (when
+**  finishes).
+**  
+**  So the following rules must be observed by the algorithm:
+**		1.- The first character (not space on a map) must be a 1 (wall).
+**		2.- The character before the next space (empty) after
+**			that 1, must be a 1 (wall).
+**		3.- The character after that space, that is not a space, has to
+**			be a 1 (wall).
+**		4.- And so on. 
+**		5.- It must be taken into account that the last character of the
+**			line that is not a space, must necessarily be a 1 (wall).
+**
+**  The program uses 2 algorithms (horizontal / vertical lines).
+*/
+
+
+void	invalid_map_hor(const char *map)
+{
+	size_t	len;
+	size_t	j;
+
+	len = ft_strlen(map);
+	j = 0;
+	if (map[0] == '\0')
+		return ;
+	if ((map[0] != '1' && ft_isblank(map[0]) == 0)
+				|| (map[len - 1] != '1' && ft_isblank(map[len - 1]) == 0))
+		msg_err("Ops, this map is open. Review it or try with other map.");
+	while (j < len)
+	{
+		while (ft_isblank(map[j]))
+			j++;
+		if (map[j] != '1' && map[j] != '\0')
+			msg_err("Ops, this map is open. Review it or try with other map.");
+		while (ft_isblank(map[j]) == 0)
+			j++;
+		if (map[j] == '1')
+			msg_err("Ops, this map is open. Review it or try with other map.");
+	}
+}
+
 
 /*
 ** Add chars N, S, W and E from map.
@@ -30,6 +82,10 @@ static void	repeat_chr(char c)
 		msg_err("Sorry, I can't accept two players.");
 }
 
+/*
+** Check chars forbidden on map
+*/
+
 void	forbidd_chr(char c)
 {
 	char	*chr_allowed;
@@ -47,6 +103,11 @@ void	forbidd_chr(char c)
 	msg_err("Forbidden characters in map");
 }
 
+/*
+**  Check some errors: chars forbidden, more than ine player and
+**  init the horizontal algorithm to check invalid maps
+*/
+
 void 	review_map_horiz(t_cub3d *a, int i, int j)
 {
 	char	*map;
@@ -56,11 +117,6 @@ void 	review_map_horiz(t_cub3d *a, int i, int j)
 	imax = a->fconf.map.row -1;
 	jmax = a->fconf.map.col -1;
 	map = a->fconf.map.map[i];
-//	printf("imax: |%d|\n", imax);
-//	printf("jmax: |%d|\n", jmax);
-//	printf(" map: |%s|\n", map);
-//	printf("   i: |%d|\n", i);
-//	printf("   j: |%d|\n", j);
 	while (i <= imax)
 	{
 		map = a->fconf.map.map[i];
@@ -69,15 +125,13 @@ void 	review_map_horiz(t_cub3d *a, int i, int j)
 		{
 			if (ft_isprint(map[j]) == 1)
 			{
-//				printf("map[%d]  pre: |%c|\n", j, map[j]);
 				forbidd_chr(map[j]);
-//				printf("map[%d] post: |%c|\n", j, map[j]);
 				repeat_chr(map[j]);
 			}
 			j++;
 		}
+		invalid_map_hor(map);
 		i++;
-//		map = a->fconf.map.map[i];
 	}
 }
 
