df9fc345c54d29422faf8a173546a9e33654fd0f
samuel <ssacrist@c1r16s2.42madrid.com>
2020-11-22 20:44:53 +0100

    It runs and free memory of struct. But 9 test KO

--- cub3d.c
+++ cub3d.c
@@ -6,7 +6,7 @@
 /*   By: ssacrist <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/05 11:17:00 by ssacrist          #+#    #+#             */
-/*   Updated: 2020/11/22 03:49:56 by ssacrist         ###   ########.fr       */
+/*   Updated: 2020/11/22 20:38:10 by ssacrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,15 @@ int		main(int argc, char **argv)
 		a = (t_cub3d *)malloc(sizeof(t_cub3d));
 		init_struct(a);
 		open_fconfig(argv[1], a);
+		if (a)
+		{
+			if (sizeof(a->fconf.wall_texture) >= 848)
+				ft_delmatrix(a->fconf.wall_texture);
+			if (sizeof(a->fconf.map.map) >= 208)
+				ft_delmatrix(a->fconf.map.map);
+			if (sizeof(a) >= sizeof(t_cub3d))
+				free(a);
+		}
 	}
 	else
 		msg_err("Revier your fingers, please.");
